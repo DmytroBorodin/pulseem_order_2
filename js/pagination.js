ff8a651d8832daacb47a632d154bcc4f3985b790
DmytroBorodin <dimabor96@gmail.com>
2021-07-06 11:39:24 +0300

    pagination fixes

--- js/pagination.js
+++ js/pagination.js
@@ -1,51 +1,55 @@
-const tableRows = [...document.querySelectorAll(".table__row__wrapper")];
-const mobTableRows = [...document.querySelectorAll(".mobile__table__row")];
-const pageSizeSelector = document.querySelector(".quantity");
-const pageSelector = document.querySelector(".page__num__btn");
+const wrappers = [...document.querySelectorAll(".main__content__wrap")];
 
-tableRows.shift();
+wrappers.forEach((wrapper) => {
+  const tableRows = [...wrapper.querySelectorAll(".table__row__wrapper")];
+  const mobTableRows = [...wrapper.querySelectorAll(".mobile__table__row")];
+  const pageSizeSelector = wrapper.querySelector(".quantity");
+  const pageSelector = wrapper.querySelector(".page__num__btn");
 
-function changePageSize(arr, pageSize) {
-  arr.forEach((elem) => {
-    let elemIndex = arr.indexOf(elem);
-    if (elemIndex >= pageSize) {
-      arr[elemIndex].style.display = "none";
-    } else {
-      arr[elemIndex].style.display = "block";
-    }
-  });
-}
+  tableRows.shift();
 
-function changePage(arr, pageSize, currentPage) {
-  let pages = arr.length / pageSize;
-  if (currentPage > pages) {
-    currentPage = pages;
+  function changePageSize(arr, pageSize) {
+    arr.forEach((elem) => {
+      let elemIndex = arr.indexOf(elem);
+      if (elemIndex >= pageSize) {
+        arr[elemIndex].style.display = "none";
+      } else {
+        arr[elemIndex].style.display = "block";
+      }
+    });
   }
-  let max = pageSize * currentPage;
-  let min = max - pageSize;
-  arr.forEach((elem) => {
-    let elemIndex = arr.indexOf(elem);
-    if (elemIndex >= min && elemIndex < max) {
-      arr[elemIndex].style.display = "block";
-    } else {
-      arr[elemIndex].style.display = "none";
+
+  function changePage(arr, pageSize, currentPage) {
+    let pages = arr.length / pageSize;
+    if (currentPage > pages) {
+      currentPage = pages;
     }
+    let max = pageSize * currentPage;
+    let min = max - pageSize;
+    arr.forEach((elem) => {
+      let elemIndex = arr.indexOf(elem);
+      if (elemIndex >= min && elemIndex < max) {
+        arr[elemIndex].style.display = "block";
+      } else {
+        arr[elemIndex].style.display = "none";
+      }
+    });
+  }
+
+  changePageSize(tableRows, 6);
+  changePageSize(mobTableRows, 6);
+  changePage(tableRows, 6, 1);
+  changePage(mobTableRows, 6, 1);
+
+  pageSizeSelector.addEventListener("change", (e) => {
+    let pageSize = e.target.value;
+    changePageSize(tableRows, pageSize);
+    changePageSize(mobTableRows, pageSize);
   });
-}
 
-changePageSize(tableRows, 6);
-changePageSize(mobTableRows, 6);
-changePage(tableRows, 6, 1);
-changePage(mobTableRows, 6, 1);
-
-pageSizeSelector.addEventListener("change", (e) => {
-  let pageSize = e.target.value;
-  changePageSize(tableRows, pageSize);
-  changePageSize(mobTableRows, pageSize);
-});
-
-pageSelector.addEventListener("change", (e) => {
-  let pageSize = pageSizeSelector.value;
-  changePage(tableRows, pageSize, +e.target.value);
-  changePage(mobTableRows, pageSize, +e.target.value);
+  pageSelector.addEventListener("change", (e) => {
+    let pageSize = pageSizeSelector.value;
+    changePage(tableRows, pageSize, +e.target.value);
+    changePage(mobTableRows, pageSize, +e.target.value);
+  });
 });
